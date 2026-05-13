3a69d7d8b365201fb1b4c07c83f4a8ec9b7a717a
Gabriele Di Muccio <gabrieledimuccio@gmail.com>
2026-05-13 15:34:57 +0200

    terminato selfwork OOP5

--- class.php
+++ class.php
@@ -1,3 +1,30 @@
 <?php
+abstract class Category{
+abstract public function getMyCategory();
 
+}
+
+class Attualità extends Category{
+public function getMyCategory(){
+    return "Attualità \n";
+}
+}
+
+class Sport extends Category{
+public function getMyCategory(){
+    return "Sport \n";
+}
+
+}class Gossip extends Category{
+public function getMyCategory(){
+    return "Gossip \n";
+}
+
+}class Storia extends Category{
+public function getMyCategory(){
+    return "Storia \n";
+}
+}
+$gossip = new Gossip();
+echo $gossip -> getMyCategory();
 ?>
